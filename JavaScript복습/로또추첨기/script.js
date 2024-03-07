332d70f033cfde2e8dac52b3bcc9f3e7460fe64e
Kamuie99 <dakgoo02@naver.com>
2024-03-07 14:45:48 +0900

    240307

--- JavaScript복습/로또추첨기/script.js
+++ JavaScript복습/로또추첨기/script.js
@@ -10,10 +10,28 @@ while (candidate.length > 0) {  // 피셔-예이츠 셔플 알고리즘
 const winBalls = shuffle.slice(0, 6).sort((a, b) => a - b);
 const bonus = shuffle[6];
 
+function colorize(number, $tag) {
+  if (number < 10) {
+    $tag.style.backgroundColor = 'red';
+    $tag.style.color = 'white';
+  } else if (number < 20){
+    $tag.style.backgroundColor = 'orange';
+  } else if (number < 30){
+    $tag.style.backgroundColor = 'yellow';
+  } else if (number < 40){
+    $tag.style.backgroundColor = 'blue';
+    $tag.style.color = 'white';
+  } else {
+    $tag.style.backgroundColor = 'green';
+    $tag.style.color = 'white';
+  }
+}
+
 const $result = document.querySelector('#result');
 function drawBall(number, $parent) {
   const $ball = document.createElement('div');
   $ball.className = 'ball';
+  colorize(number, $ball);
   $ball.textContent = number;
   $parent.appendChild($ball);
 }
